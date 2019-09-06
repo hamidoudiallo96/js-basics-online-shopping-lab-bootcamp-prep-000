f286f68558ad48132fb4e0c840722767a5be3858
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-06 06:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const removeFromCart =item=>{
   }else{
     for(let i = 0; i<cart.length; i++){
       if(item === cart[i].itemName){
-        delete cart[i].itemName
+        cart.splice(i,1)
         return cart
       }
     }
