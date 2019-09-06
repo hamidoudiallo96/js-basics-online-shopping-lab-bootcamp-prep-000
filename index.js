8b5af1f5f8494d9c7e162b9e127b58fadf4700b0
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-06 06:54:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,22 +18,18 @@ const addToCart = (item) =>{
 
 
 const viewCart = ()=>{
-  let sentenceStart = "In your cart, you have "
-  let sentenceEnd = `and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
-  if(cart.length === 0){
-    return "Your shopping cart is empty"
-  }
-  if(cart.length === 1){
-    sentenceStart+= `${cart[0].itemName} at $${cart[0].itemPrice}.`
-    return sentenceStart
-  }else if(cart.length === 2){
-    sentenceStart+=`${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-      return sentenceStart
-  }else{
-    for(let i= 0; i<cart.length-1; i++){
-      sentenceStart+= `${cart[i].itemName} at $${cart[i].itemPrice}, `
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    var myString = "In your cart, you have "
+    for(var i = 0; i < cart.length - 1; i++) {
+      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
-    return sentenceStart + sentenceEnd
+    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
   }
 }
 
