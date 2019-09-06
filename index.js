428c80acfa89f27217fd36de6b8fc130f5a8c519
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-06 06:41:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ const addToCart = (item) =>{
 
 
 const viewCart = ()=>{
+  let sentenceStart = "In your cart, you have "
+  let sentenceEnd = ` and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
   if(cart.length === 0){
     return "Your shopping cart is empty"
   }
-  if(cart)
+  if(cart.length === 1){
+    sentenceStart+= `${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }else if(cart.length === 2){
+    sentenceStart+=`${cart[0].itemName} at $${cart[0].itemPrice}, and $${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }else{
+    for(let i= 0; i<cart.length; i++){
+      sentenceStart+= `${cart[0].itemName} at $${cart[0].itemPrice},`
+    }
+  }
+  return sentenceStart
 }
 
 
